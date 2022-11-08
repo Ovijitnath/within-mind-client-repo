abf80e5f1244826bb87168b441b45cfe4c34c30b
Ovijitnath <ovijitnathgn@gmail.com>
2022-11-09 03:56:37 +0600

    header issue fixed

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -41,7 +41,7 @@ const Item = ({ title, children }) => {
 
 export const Blog = () => {
     return (
-        <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
                 <div className="flex flex-col mb-16 sm:text-center">
                     <a href="/blog" className="mb-6 sm:mx-auto">
@@ -86,7 +86,7 @@ export const Blog = () => {
                                         height="24"
                                     />
                                 </svg>
-                                <span class="relative">The</span>
+                                <span className="relative">The</span>
                             </span>{' '}
                             Blog Page
                         </h2>
--- src/Pages/SharedPage/Header/Header.js
+++ src/Pages/SharedPage/Header/Header.js
@@ -7,38 +7,38 @@ const Header = () => {
         <div>
 
             <div>
-                <nav class=" bg-gray-100  shadow ">
-                    <div class="max-w-7xl mx-auto px-8">
-                        <div class="flex items-center justify-between h-16">
-                            <div class="w-full justify-between flex items-center">
+                <nav className=" bg-gray-100  shadow ">
+                    <div className="max-w-7xl mx-auto px-8">
+                        <div className="flex items-center justify-between h-16">
+                            <div className="w-full justify-between flex items-center">
                                 <Link to="/" className="btn btn-ghost normal-case not-italic hover:italic font-black text-xl"><img src={logo} style={{ width: 30, height: 30 }} alt="" />   Within Mind</Link>
-                                <div class="hidden md:block">
-                                    <div class="ml-10 flex items-baseline space-x-4">
-                                        <Link class="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/home">
+                                <div className="hidden md:block">
+                                    <div className="ml-10 flex items-baseline space-x-4">
+                                        <Link className="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/home">
                                             Home
                                         </Link>
-                                        <Link class="text-gray-800  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/services">
+                                        <Link className="text-gray-800  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/services">
                                             Services
                                         </Link>
-                                        <Link class="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/blog">
+                                        <Link className="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/blog">
                                             Blog
                                         </Link>
-                                        <Link class="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/contact">
+                                        <Link className="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/contact">
                                             Contact
                                         </Link>
-                                        <Link class="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/login">
+                                        <Link className="text-gray-500  hover:text-indigo-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/login">
                                             Login
                                         </Link>
                                     </div>
                                 </div>
                             </div>
-                            <div class="block">
-                                <div class="ml-4 flex items-center md:ml-6">
+                            <div className="block">
+                                <div className="ml-4 flex items-center md:ml-6">
                                 </div>
                             </div>
-                            <div class="-mr-2 flex md:hidden">
-                                <button class="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
-                                    <svg width="20" height="20" fill="currentColor" class="h-8 w-8" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                            <div className="-mr-2 flex md:hidden">
+                                <button className="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+                                    <svg width="20" height="20" fill="currentColor" className="h-8 w-8" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
                                         </path>
                                     </svg>
@@ -46,21 +46,21 @@ const Header = () => {
                             </div>
                         </div>
                     </div>
-                    <div class="md:hidden">
-                        <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                            <Link class="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/home">
+                    <div className="md:hidden">
+                        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                            <Link className="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/home">
                                 Home
                             </Link>
-                            <Link class="text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium" to="/services">
+                            <Link className="text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium" to="/services">
                                 Services
                             </Link>
-                            <Link class="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/blog">
+                            <Link className="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/blog">
                                 Blog
                             </Link>
-                            <Link class="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/contact">
+                            <Link className="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/contact">
                                 Contact
                             </Link>
-                            <Link class="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/login">
+                            <Link className="text-gray-500 hover:text-indigo-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to="/login">
                                 Login
                             </Link>
                         </div>
