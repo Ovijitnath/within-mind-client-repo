a5edcd8e04eef44c8afedc9a758a9bb4238a41ba
Ovijitnath <ovijitnathgn@gmail.com>
2022-11-10 01:04:22 +0600

    Auth added

--- src/Pages/Registration/Registration.js
+++ src/Pages/Registration/Registration.js
@@ -1,40 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Form, Link } from 'react-router-dom';
 
 const Registration = () => {
+
+    const handleSignUp = event => {
+        event.preventDefault();
+    }
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
 
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <Form onSubmit={handleSignUp} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="Full Name" className="input input-bordered" />
+                                <input name="name" type="text" placeholder="Full Name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input name="email" type="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input name="password" type="password" placeholder="password" className="input input-bordered" required />
 
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Register</button>
                             </div>
                             <p className="text-xm text-center sm:px-6 dark:text-gray-400">Already have an account?
-                                <Link rel="noopener noreferrer" to="/login" className="underline dark:text-gray-100">    Log in</Link>
+                                <Link rel="noopener noreferrer" to="/login" className="underline text-cyan-300">    Log in</Link>
                             </p>
-                        </div>
+                        </Form>
                     </div>
 
                     <div className="text-center lg:text-left">
--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
 const Service = ({ service }) => {
-    const { img, price, title, description } = service
+    // const { img, price, title, description } = service;
     return (
+
+        // <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        //     <figure><img src={img} alt="Shoes" /></figure>
+        //     <div className="card-body">
+        //         <h2 className="card-title">{title}</h2>
+        //         <h4>$  {price}</h4>
+        //         <p>{description.slice(0, 100)}</p>
+        //         <div className="card-actions justify-end">
+        //             <button className="btn btn-primary">Buy Now</button>
+        //         </div>
+        //     </div>
+        // </div>
         <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src={img} alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
-                    <h4>$  {price}</h4>
-                    <p>{description.slice(0, 100)}</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+            <h1>hi</h1>
+            <h2>I</h2>
+            <h3>am</h3>
         </div>
+
     );
 };
 
